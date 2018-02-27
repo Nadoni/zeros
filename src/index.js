8c0cda739c22eab3a1ac025a569c7f2bfdfa26ab
kojaniydracon <kojaniydracon@gmail.com>
2018-02-27 20:39:23 +0300

    zerosed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+
+    let count = 0;
+    let multifive = 5;
+
+
+    while (number/multifive >= 1){
+        count += Math.trunc(number/multifive);
+        multifive *= 5;
+    }
+
+    return count;
 }
